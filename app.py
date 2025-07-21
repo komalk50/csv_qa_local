d4079a228860d56841c3a406d9938e3b128ce9e1
komalk50 <komalv55555@gmail.com>
2025-07-21 19:34:30 +0530

    updation in query search

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from qa_engine import CSV_QA_Engine
 
 st.set_page_config(page_title="Offline CSV Q&A App", layout="centered")
-st.title(" CSV Q&A (Offline with Ollama + LangChain)")
+st.title("CSV Q&A (Offline with Ollama + LangChain)")
 
 uploaded_file = st.file_uploader("Upload your CSV", type="csv")
 
@@ -18,4 +18,8 @@ if uploaded_file:
             qa = CSV_QA_Engine()
             qa.load_and_prepare(df)
             answer = qa.answer(question)
-            st.success(answer)
+
+            if isinstance(answer, pd.DataFrame):
+                st.dataframe(answer)
+            else:
+                st.success(answer)
